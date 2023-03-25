374169d6d921f1b1b3365fa2e84a8d4e40c233d6
piotrv1001 <piotrv1001@interia.pl>
2023-03-25 09:47:58 +0100

    Register screen design

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -7,12 +7,13 @@ import { AuthService } from "src/app/shared/auth/auth.service";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../login/login.component.scss']
 })
 export class RegisterComponent {
 
   @Output() loginBtnClick: EventEmitter<void> = new EventEmitter<void>();
   registerForm?: FormGroup;
+  doPasswordsMatch = true;
 
   constructor(
     private fb: FormBuilder,
@@ -26,27 +27,35 @@ export class RegisterComponent {
       return this.registerForm?.get('password')?.value;
     }
 
+    get confirmedPassword(): string {
+      return this.registerForm?.get('confirmPassword')?.value;
+    }
+
     ngOnInit(): void {
       this.registerForm = this.fb.group({
         username: '',
-        password: ''
+        password: '',
+        confirmPassword: ''
       });
     }
 
     register(): void {
-      const userDto = new UserDTO(
-        this.username, this.password
-      );
-      this.authService.register(userDto).pipe(
-        switchMap(() => this.authService.login(userDto))
-      ).subscribe({
-        next: () => {
-          this.authService.authenticate();
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      })
+      this.doPasswordsMatch = (this.password === this.confirmedPassword) && (this.password !== '' || this.confirmedPassword !== '');
+      if(this.doPasswordsMatch) {
+        const userDto = new UserDTO(
+          this.username, this.password
+        );
+        this.authService.register(userDto).pipe(
+          switchMap(() => this.authService.login(userDto))
+        ).subscribe({
+          next: () => {
+            this.authService.authenticate();
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        })
+      }
     }
 
     loginBtnClicked(): void {
